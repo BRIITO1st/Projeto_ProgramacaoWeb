9457ea57c11e5147afe45a457597c9a97d7f4d8a
BRIITO1st <igor.fb003@gmail.com>
2024-09-19 08:32:59 -0300

    ultima atualizacao

--- index.js
+++ index.js
@@ -1,45 +1,86 @@
-const diaSemana = document.getElementById("Dia da semana");
-const diaMesAno = document.getElementById("Dia-mes-ano");
-const HoraMinSeg = document.getElementById("Hora-minuto-segundo");
-const arrayDayWeek = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"]
+// TO-DO:
+// Organizar código
+
+const diaSemana = document.getElementById("dia-semana");
+const diaMesAno = document.getElementById("dia-mes-ano");
+const horaMinSeg = document.getElementById("hora-min-seg");
+const arrayDayWeek = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sabado"]
 
 
 const dialogPonto = document.getElementById("dialog-ponto");
-dialogPonto.showModal();
 
+
+navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+});
+
+
+// TO-DO:
+// apresentar para o usuário a data e hora atualizados
+// atualizar a data todos os dias 00:00
+// atualizar a hora todo segundo
 const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
-btnRegistrarPonto.addEventListener("click", ()=>{
+btnRegistrarPonto.addEventListener("click", () => {
     dialogPonto.showModal();
 });
 
-const btnFuncaoFechar = document.getElementById("btn-funcao-fechar");
-btnFuncaoFechar.addEventListener("click", ()=>{
-    dialogPonto.close();
-})
 
-//Todo conjunto numerico (exceto ano) deve ter 2 digitos (adicionar )
-// Retornar dia da semana por extenso (em pt-br)
+const btnDialogFechar = document.getElementById("btn-dialog-fechar");
+btnDialogFechar.addEventListener("click", () => {
+    dialogPonto.close();
+});
+
+
+const btnDialogRegistrarPonto = document.getElementById("btn-dialog-registrar-ponto");
+btnDialogRegistrarPonto.addEventListener("click", () => {
+    let data = dataCompleta();
+    let hora = horaCompleta();
+    let tipoPonto = document.getElementById("select-tipos-ponto").value;
+
+    let ponto = {
+        "data": data,
+        "hora": hora,
+        "tipo": tipoPonto,
+        "id": 1
+    }
+
+    // TO-DO:
+    // Somente o ultimo registro está sendo salvo
+    // como resolver isso, de modo que eu persista todos os pontos?
+    localStorage.setItem("registro", JSON.stringify(ponto));
+
+    // TO-DO:
+    // salvar o útimo tipo do ponto registrado pelo usuário
+    // fazer o select considerar esse último ponto e selecionar, por padrão
+    // o próximo possível ponto do usuário
+    // Exemplo: usuário registrou "entrada", determinar que o select apresente "intervalo" como valor padrão
+
+    console.log(ponto);
+    
+});
 
 function daySemana() {
-    //retornar dia da semana
     const date = new Date();
-    return arrayDayWeek [date.getDay()]
+    return arrayDayWeek[date.getDay()];
 }
 
-function dataCompleta(){
+function dataCompleta() {
     const date = new Date();
-    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-}
-function horaCompleta(){
-    const date = new Date();
-    return date.getHours() + ":" + date.getMinutes + ":" + date.getSeconds();
+    return String(date.getDate()).padStart(2, '0') + "/" + String(date.getMonth() + 1).padStart(2, '0') + "/" + date.getFullYear();
 }
 
-function atualizaHora(){
-    HoraMinSeg.textContent = horaCompleta();
+function horaCompleta() {
+    const date = new Date();
+    return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
 }
 
-setInterval(atualizaHora, 1000)
+function atualizaHora() {
+    horaMinSeg.textContent = horaCompleta();
+}
+
+setInterval(atualizaHora, 1000);
 
 diaSemana.textContent = daySemana();
 diaMesAno.textContent = dataCompleta();
